40585faf4480dacdec1bd0fb11bd8e5c818ad16e
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-16 16:41:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
-
-def countdown
+def countdown(n)
+  while n>0
+        countdown_output = "#{n} SECOND(S)!\n"
+        puts countdown_output
+        n-=1
+  end
+ return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(x)
+  x=10
+    while x>1
+      sleep 1
+      x-=1
+    end
 end
