fc3b3d123bf4440fb59ae9dd344f35ae525fdc1e
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-22 15:37:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,9 +7,11 @@ def countdown(n)
   end
  return "HAPPY NEW YEAR!"
 end
-def countdown_with_sleep(x)
-  x=10
-    while x>1
+def countdown_with_sleep(n)
+  x=0
+    while x > n.length
+      countdown_output = "#{n} SECOND(S)!\n"
+      puts countdown_output
       sleep 1
       x-=1
     end
